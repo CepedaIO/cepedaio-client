d956b0074512103b23c48e7d9535cdb586dcb1a2
Alfred Cepeda <alfred@cepeda.io>
2022-06-04 01:16:53 -0400

    Adds router

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,38 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <main class="flex flex-col h-full">
+    <router-view></router-view>
+
+    <nav class="flex flex-row justify-around w-full mt-auto">
+      <router-link
+        v-for="button in navigation"
+        :key="button.url"
+        :to="button.url"
+        class="flex-grow flex"
+      >
+        <div class="py-5 mx-auto">
+          <i class="fas fa-home fa-3x" :class="[button.icon]"></i>
+        </div>
+      </router-link>
+    </nav>
+  </main>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
 
 export default defineComponent({
   name: "App",
-  components: {
-    HelloWorld,
+  data: () => {
+    return {
+      navigation: [
+        { icon: "fa-home", url: "/" },
+        { icon: "fa-toolbox", url: "/tools" },
+        { icon: "fa-blog", url: "/blog" },
+      ],
+    };
   },
 });
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/pages/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <h1 class="mb-20">Welcome to AJ's Website!</h1>
+
+  <p>Hi! This is AJ's website</p>
+</template>
+
+<script lang="ts">
+import { ref, defineComponent } from "vue";
+export default defineComponent({
+  name: "HelloWorld",
+  setup: () => {
+    const count = ref(0);
+    return { count };
+  },
+});
+</script>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+
+label {
+  margin: 0 0.5em;
+  font-weight: bold;
+}
+
+code {
+  background-color: #eee;
+  padding: 2px 4px;
+  border-radius: 4px;
+  color: #304455;
+}
+</style>
